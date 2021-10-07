c7aa2f5333400a050044eea17b0377bdeaee733b
Mehmet Akif Inan <72706177+Mehmet-akif-inan@users.noreply.github.com>
2021-10-07 21:46:29 +0300

    Switch example
    Switch example with Java

---
+++ SwitchDemo/src/Main.java
@@ -0,0 +1,30 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		char grade = 'F';
+		
+		switch(grade) {
+		case 'A':
+			System.out.println("You passed the exam.");
+			break;
+		case 'B':
+			System.out.println("You passed the exam.");
+			break;
+		case 'C':
+			System.out.println("You passed the exam.");
+			break;
+		case 'D':
+			System.out.println("Maybe you passed the exam.");
+			break;
+		case 'E':
+			System.out.println("You failed the exam.");
+			break;
+		case 'F':
+			System.out.println("You should leave school");
+			break;
+		}
+
+	}
+
+}
